0b21268113ddd03b47f995df635e1b72827417b2
Aro-PC <arowhakobyan@gmail.com>
2017-06-16 17:47:23 +0400

    anim helper

---
+++ animationhelperlibrary/src/main/java/com/example/animationhelperlibrary/ColorAnimation.java
@@ -0,0 +1,12 @@
+package com.example.animationhelperlibrary;
+
+import com.google.android.gms.maps.model.Polyline;
+
+/**
+ * Created by Aro-PC on 6/16/2017.
+ */
+
+public class ColorAnimation {
+    public ColorAnimation(Polyline polyline, int fromColor, int toColor) {
+    }
+}
